40a1dbca77a73ff5944f413de5303b2230281434
bethany <bethanylynnpro@gmail.com>
2023-06-16 09:57:53 -0700

    added svg icons for links

--- src/BottomBar.js
+++ src/BottomBar.js
@@ -5,10 +5,10 @@ function BottomBar() {
   return (
     <div className="bottom-bar">
       <a href="https://www.linkedin.com/in/bethanylynnpro/" target="_blank" rel="noopener noreferrer">
-        <img src="linkedin.png" alt="Bethany's LinkedIn" />
+        <img src="https://www.svgrepo.com/show/354000/linkedin-icon.svg" alt="Bethany's LinkedIn" />
       </a>
       <a href="https://github.com/bethany-lynn" target="_blank" rel="noopener noreferrer">
-        <img src="github.png" alt="Bethany's GitHub" />
+        <img src="https://www.svgrepo.com/download/35001/github.svg" alt="Bethany's GitHub" />
       </a>
     </div>
   );
