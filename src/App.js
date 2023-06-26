024e3758c6cd38bae313a244d99eb5abe0f64bf6
bethany <bethanylynnpro@gmail.com>
2023-06-26 14:59:34 -0700

    saved old code

--- src/App.js
+++ src/App.js
@@ -104,29 +104,90 @@ function App() {
 
 export default App;
 
-// take out everything except bottom and nav bar 
-// only the things i want on specific pages should be in those files
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// working logo color customization, but not working themes for logo:
+
+// import React, { useState } from 'react';
+// import Navbar from './Navbar'; 
+// import Projects from './Projects';
+// import CareerLore from './CareerLore';
+
+// import './App.css';
+// import './Navbar.css';
+
+
+// import logo from './logo.svg';
+// import Home from './Home';
+// import BottomBar from './BottomBar';
+// import { Route, Routes } from 'react-router-dom';
+
 
 // function App() {
-//   const [backgroundColor, setBackgroundColor] = useState('#c285f5');
+//   const themes = [
+//     {
+//       name: 'Theme 1',
+//       backgroundColor: '#c285f5',
+//       textColor: '#FFC8FB',
+//       squiggleColor: '#CFBFF7',
+//       logoColors: {
+//         fill: '#1D0134',
+//         stroke: '#1D0134',
+//         iconFill: '#1D0134',
+//         iconStroke: '#CCCCCC'
+//       }
+//     },
+//     {
+//       name: 'Theme 2',
+//       backgroundColor: '#1B512D',
+//       textColor: '#B1CF5F',
+//       squiggleColor: '#BFF0D4',
+//       logoColors: {
+//         fill: '#1D0134',
+//         stroke: '#1D0134',
+//         iconFill: '#1D0134',
+//         iconStroke: '#CCCCCC'
+//       }
+//     },
+//     {
+//       name: 'Theme 3',
+//       backgroundColor: '#383B53',
+//       textColor: '#D4D6B9',
+//       squiggleColor: '#C0E0DE',
+//       logoColors: {
+//         fill: '#1D0134',
+//         stroke: '#1D0134',
+//         iconFill: '#1D0134',
+//         iconStroke: '#CCCCCC'
+//       }
+//     },
+//   ];
 
-//   const changeColors = () => {
-//     const newColor = getRandomColor();
-//     setBackgroundColor(newColor);
-//   };
+//   const [themeIndex, setThemeIndex] = useState(0);
+//   const theme = themes[themeIndex];
 
-//   const getRandomColor = () => {
-//     const letters = '0123456789ABCDEF';
-//     let color = '#';
-//     for (let i = 0; i < 6; i++) {
-//       color += letters[Math.floor(Math.random() * 16)];
-//     }
-//     return color;
+//   const changeTheme = () => {
+//     const nextIndex = (themeIndex + 1) % themes.length;
+//     setThemeIndex(nextIndex);
 //   };
 
 //   return (
-//     <div style={{ backgroundColor }}>
-//     <Navbar />
+//     <div style={{ backgroundColor: theme.backgroundColor, color: theme.textColor }}>
+//     <Navbar squiggleColor={theme.squiggleColor} />
 //       <Routes>
 //         <Route path="/" element={<Home />}/>
 //         <Route path="/projects" element={<Projects />} />
@@ -134,14 +195,17 @@ export default App;
 //       </Routes>
 //     <div className="App">
 //       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
+//       <img src={logo} className={`App-logo ${theme.logoClassName}`} alt="logo" style={theme.logoColors} />
+
 //         <p>
 //           {/* things here show up on every page */}
 //         </p>
 //       </header>
 //       <BottomBar />
-//       <button onClick={changeColors}>Change Color</button>
+//       <button onClick={changeTheme}>im bored</button>
 //     </div>
 //     </div>
 //   );
 // }
+
+// export default App;
