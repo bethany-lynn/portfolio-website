9ea9f700ac6c6f6a7ddc022f8c7e374ce99134d5
bethany <bethanylynnpro@gmail.com>
2023-06-27 15:30:05 -0700

    finished text gradient animation

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,27 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
+
     return (
       <div className="home-container">
           <div className="content-container-header">
             <h1>Bethany Bear </h1>
           </div>
           <div className="content-container-body">
-            Full Srack Software Engineer
+            Full Stack Software Engineer
           </div>
     </div>
     );
   }
   
   export default Home;  
+
+//   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+//   <defs>
+//     <linearGradient id="gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+//       <stop offset="0%" stopColor="#a5f675" />
+//       <stop offset="100%" stopColor="#e7ad6a" />
+//     </linearGradient>
+//   </defs>
+//   <text x="0" y="50" fontSize="24" fontFamily="Quicksand" fill="url(#gradient)">testing</text>
+// </svg>
