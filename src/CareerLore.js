f7e3b0e4b2e01cc039605b569a86869dc8c8b68b
bethany <bethanylynnpro@gmail.com>
2023-07-01 10:28:56 -0700

    added embedded resume pdf

--- src/CareerLore.js
+++ src/CareerLore.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './CareerLore.css'
+// import { Document, Page } from 'react-pdf';
+// import ResumePDF from '../public/resume.pdf'
 
 function CareerLore() {
     return (
         <div className='career-lore-body'>
-            <h1>Career Lore</h1>
-            information about what ive done before
-            <br /> add pdf of resume and cover letter
+          <h1>Career Lore</h1>
+            <iframe src="https://drive.google.com/file/d/1jyAruxzs4i4Q3RpT3L-rRXDFToODz7wH/preview" 
+            title="Embedded PDF" 
+            width="540" height="750" allow="autoplay">
+            </iframe>
         </div>
     );
 }
