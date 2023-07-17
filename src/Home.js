1b070369bdf4c1a69800d6dd36defc31594614bf
bethany <bethanylynnpro@gmail.com>
2023-07-17 13:26:55 -0700

    added photos - edited home flex box

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './Home.css';
-// import myFace from '.jpg'
+import myFace from './images/self.jpg'
 
 function Home({ appLogo }) {
   return (
     <div className="home-container">
       <div className="header-content">
-        <h1 className='name'>Bethany Winter</h1>
+        <h1 className="name">Bethany Winter</h1>
       </div>
-      <div className="content-container-body">
-        Full Stack Software Engineer
-        <br />
-        <div className='email'>
-          bethanylynnpro@gmail.com
-          {/* <img src={myFace} alt='Bethanys face' className='my-image' /> */}
-        </div> {appLogo}
+      <div className="content-container-box">
+        <div className="column-one">
+          <div className="job-title">
+            Full Stack Software Engineer
+          </div>
+          <div className="email">
+            bethanylynnpro@gmail.com
+          </div>
+          <div className='applogo'>
+            {appLogo}
+          </div>
+        </div>
+        <div className="column-two">
+          <img src={myFace} alt="Bethany's face" />
+        </div>
       </div>
     </div>
   );
