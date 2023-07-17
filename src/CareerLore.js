297c8fb17e1c8bd8b1d274ab78b349f3a2532e03
bethany <bethanylynnpro@gmail.com>
2023-07-17 09:48:51 -0700

    debugged career cards

--- src/CareerLore.js
+++ src/CareerLore.js
@@ -4,10 +4,10 @@ import './CareerLore.css'
 function CareerLore() {
   return (
     <div className='card'>
-      <h1 className='card-title' >How I Got Here</h1>
       <div className="card-content">
-        At the age of 17, I went to beauty school, and 
-        <br/>immediately knew I wanted to work for myself, or away from the grind-culture of the beauty industry.
+        <h1 className='card-title' >How I Got Here</h1>
+        At the age of 17, I went to beauty school, and
+        <br />immediately knew I wanted to work for myself, or away from the grind-culture of the beauty industry.
         <br />
         I worked very hard for half a dozen years to build my own business,
         community, and brand for myself.
@@ -16,7 +16,7 @@ function CareerLore() {
         While I loved many aspects of my career, especially the creative side and the freedom to
         create my own schedule,
         <br />
-        I struggled to find work-life balance. My home life is important to me, 
+        I struggled to find work-life balance. My home life is important to me,
         <br />
         and when the pandemic lockdown hit, all of my struggles and goals came to a head.
         <br />
@@ -34,10 +34,10 @@ function CareerLore() {
         I enjoy coding, and I really enjoy the idea of using my skills and knowledge to create my actual dream
         life: relaxing with my dog and visiting friends and family across the county.
         <br />
-      </div>
+      </div> <br />
       <div className="card-content">
         <h1 className='card-title'>My Aspirations and Values</h1>
-        I want to work for a company that sees technology as a tool to improve daily life. 
+        I want to work for a company that sees technology as a tool to improve daily life.
         That can look like a lot of different things, but progress should exist
         <br />
         <br />
@@ -47,21 +47,21 @@ function CareerLore() {
         <br />
         <br />
         For some people, that is work. For me, that looks like a lot of time enjoying a cup of tea in nature, with my dog,
-        <br /> 
+        <br />
         maybe by a bonfire, and seeing all corners of the world.
         <br />
         Community, inclusivity, kindness, and the earth are my biggest passions, and I'd love to work
-        with people who felt similarly. 
-      </div>
+        with people who felt similarly.
+      </div> <br />
       <div className="card-content">
         <h1 className='card-title'>Free Time</h1>
         In my free time, I do drink a hefty amount of tea, and I'm usually drinking it while playing a video game,
         doing a puzzle, reading a book, or watching anime. Since most of my hobbies are sedentary, I try to get outside
-        <br /> 
+        <br />
         with my pup, Satoru, and my partner, Max, a few times a day, and out to our local park a couple times a week.
         <br />
         <br />
-        We both enjoy the sound and sight of birds and water. We hope to one day invest in an RV, remodel it, and use it to 
+        We both enjoy the sound and sight of birds and water. We hope to one day invest in an RV, remodel it, and use it to
         <br />
         drive all over the country, and visit every forest we can!
       </div>
