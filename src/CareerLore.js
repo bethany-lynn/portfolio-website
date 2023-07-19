1047ca08fbe315bcef71893879601fd1d700d829
bethany <bethanylynnpro@gmail.com>
2023-07-19 14:14:09 -0700

    responsive design for career lore

--- src/CareerLore.js
+++ src/CareerLore.js
@@ -4,66 +4,49 @@ import './CareerLore.css'
 function CareerLore() {
   return (
     <div className='card'>
-      <div className="card-content">
-        <h1 className='card-title' >How I Got Here</h1>
-        At the age of 17, I went to beauty school, and
-        <br />immediately knew I wanted to work for myself, or away from the grind-culture of the beauty industry.
-        <br />
-        I worked very hard for half a dozen years to build my own business,
-        community, and brand for myself.
-        <br />
-        <br />
-        While I loved many aspects of my career, especially the creative side and the freedom to
-        create my own schedule,
-        <br />
-        I struggled to find work-life balance. My home life is important to me,
-        <br />
-        and when the pandemic lockdown hit, all of my struggles and goals came to a head.
-        <br />
-        <br />
-        I was out of work, like many, for months. Coming back into my salon, it was a struggle to pick up
-        the pace and same income because so many things had shifted.
-        <br />
-        So I took a big chance after 13 years doing hair, moved to California from Illinois, and decided
-        to start teaching myself how to code before joining a bootcamp!
-        <br />
-        <br />
-        I loved the idea of working remotely, especially as an introvert, and the endless possibilities
-        that come with working in tech. Job assurance is essentially built-in.
-        <br />
-        I enjoy coding, and I really enjoy the idea of using my skills and knowledge to create my actual dream
-        life: relaxing with my dog and visiting friends and family across the county.
-        <br />
+      <div className="card-content-wrapper">
+        <h1 className='card-title'>How I Got Here</h1>
+        <div className="card-content">
+          At the age of 17, I went to beauty school, and
+          immediately knew I wanted to work for myself, or away from the grind-culture of the beauty industry.
+          I worked very hard for half a dozen years to build my own business,
+          community, and brand for myself.
+          While I loved many aspects of my career, especially the creative side and the freedom to
+          create my own schedule,
+          I struggled to find work-life balance. My home life is important to me,
+          and when the pandemic lockdown hit, all of my struggles and goals came to a head.
+          I was out of work, like many, for months. Coming back into my salon, it was a struggle to pick up
+          the pace and same income because so many things had shifted.
+          So I took a big chance after 13 years doing hair, moved to California from Illinois, and decided
+          to start teaching myself how to code before joining a bootcamp!
+          I loved the idea of working remotely, especially as an introvert, and the endless possibilities
+          that come with working in tech. Job assurance is essentially built-in.
+          I enjoy coding, and I really enjoy the idea of using my skills and knowledge to create my actual dream
+          life: relaxing with my dog and visiting friends and family across the county.
+        </div>
       </div> <br />
-      <div className="card-content">
+      <div className="card-content-wrapper">
         <h1 className='card-title'>My Aspirations and Values</h1>
-        I want to work for a company that sees technology as a tool to improve daily life.
-        That can look like a lot of different things, but progress should exist
-        <br />
-        <br />
-        to make life easier and more enriching for us all. Professional relationships are important and valuable,
-        <br />
-        but to me, we should use work as a jumping point to really invest in our personal lives and what brings us joy.
-        <br />
-        <br />
-        For some people, that is work. For me, that looks like a lot of time enjoying a cup of tea in nature, with my dog,
-        <br />
-        maybe by a bonfire, and seeing all corners of the world.
-        <br />
-        Community, inclusivity, kindness, and the earth are my biggest passions, and I'd love to work
-        with people who felt similarly.
+        <div className="card-content">
+          I want to work for a company that sees technology as a tool to improve daily life.
+          That can look like a lot of different things, but progress should exist
+          to make life easier and more enriching for us all. Professional relationships are important and valuable,
+          but to me, we should use work as a jumping point to really invest in our personal lives and what brings us joy.
+          For some people, that is work. For me, that looks like a lot of time enjoying a cup of tea in nature, with my dog,
+          maybe by a bonfire, and seeing all corners of the world.
+          Community, inclusivity, kindness, and the earth are my biggest passions, and I'd love to work
+          with people who felt similarly.
+        </div>
       </div> <br />
-      <div className="card-content">
+      <div className="card-content-wrapper">
         <h1 className='card-title'>Free Time</h1>
-        In my free time, I do drink a hefty amount of tea, and I'm usually drinking it while playing a video game,
-        doing a puzzle, reading a book, or watching anime. Since most of my hobbies are sedentary, I try to get outside
-        <br />
-        with my pup, Satoru, and my partner, Max, a few times a day, and out to our local park a couple times a week.
-        <br />
-        <br />
-        We both enjoy the sound and sight of birds and water. We hope to one day invest in an RV, remodel it, and use it to
-        <br />
-        drive all over the country, and visit every forest we can!
+        <div className="card-content">
+          In my free time, I do drink a hefty amount of tea, and I'm usually drinking it while playing a video game,
+          doing a puzzle, reading a book, or watching anime. Since most of my hobbies are sedentary, I try to get outside
+          with my pup, Satoru, and my partner, Max, a few times a day, and out to our local park a couple times a week.
+          We both enjoy the sound and sight of birds and water. We hope to one day invest in an RV, remodel it, and use it to
+          drive all over the country, and visit every forest we can!
+        </div>
       </div>
     </div>
   );
